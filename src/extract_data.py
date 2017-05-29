5ff2f079ba538ec0b8842963123839d1eda2954b
Gus Liu <gusliu@corn20.stanford.edu>
2017-05-29 11:26:52 -0700

    Data resizing and baseline feedforward neural network implementation

--- src/extract_data.py
+++ src/extract_data.py
@@ -3,21 +3,21 @@ import matplotlib.image as mpimg
 from PIL import Image
 import PIL
 
-def plot_images(data):
-  f = open('joint_annotation_data_scaled.txt', 'w')
+def resize_images(data):
+  f = open('../../data/images/joint_annotation_data_scaled.txt', 'w')
   for i in range(len(data)):
     try:
       line = data[i].split()
       image_name = line[0]
-      img = mpimg.imread("../data/images/" + image_name)
+      img = mpimg.imread("../../data/images/" + image_name)
       line = line[1:]
       x = line[:len(line) / 2]
       y = line[len(line) / 2:]
       
-      im = Image.open("../data/images/" + image_name)
+      im = Image.open("../../data/images/" + image_name)
       size = 480, 640
       im = im.resize(size, PIL.Image.ANTIALIAS)
-      im.save("../data/images_scaled/" + image_name)
+      im.save("../../data/images_scaled/" + image_name)
 
       newline = [image_name]
       xdim, ydim, zdim = img.shape
@@ -29,12 +29,25 @@ def plot_images(data):
       f.write(newline + "\n")
     except:
       print "Invalid file"
+  f.close()
 
+def plot_images(data, num_to_plot=5):
+  for i in range(num_to_plot):
+    line = data[i].split()
+    image_name = line[0]
+    img = mpimg.imread("../../data/images/" + image_name)
+    line = line[1:]
+    x = line[:len(line) / 2]
+    y = line[len(line) / 2:]
+
+    plt.imshow(img)
+    plt.scatter(x, y)
+    plt.show()
 
 def extract_data():
-  with open("../data/images/joint_annotation_data.txt") as f:
+  with open("../../data/images/joint_annotation_data.txt") as f:
     data = f.readlines()
-  plot_images(data)
+  resize_images(data)
 
 if __name__ == "__main__":
   extract_data()
--- src/pose_feedforward_baseline.py
+++ src/pose_feedforward_baseline.py
@@ -9,6 +9,7 @@ import numpy as np
 import tensorflow as tf 
 import glob 
 import ast 
+import random
 import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
 
@@ -23,11 +24,14 @@ USAGE_STR = """
 
 """
 
-IMAGE_DIR = "/afs/ir.stanford.edu/users/g/u/gusliu/cs231a/final_project/data/images"
+IMAGE_DIR = "/afs/ir.stanford.edu/users/g/u/gusliu/cs231a/final_project/data/images_scaled"
 ANNOTATION_FILE="/afs/ir.stanford.edu/users/g/u/gusliu/cs231a/final_project/data/images/joint_annotation_data.txt"
 
 
-def get_data():
+xdim, ydim, zdim = 640, 480, 3
+output_len = 32
+
+def get_data(num_train_pts, num_test_pts):
 	"""
 		Parse annotation to map image.jpg file to 1 x 32 vector of all x,y joint coordinates
 		Output: {img.jpg: [x1, x2, ... x16, y1, y2, ... y16]}
@@ -39,44 +43,91 @@ def get_data():
 		linfo = line.strip().split("\t")
 		image_name = linfo[0]
 		coord = map(float, linfo[1:])
-		x.append(IMAGE_DIR + "/" + image_name)
+		im_path = IMAGE_DIR + "/" + image_name
+		img_pixels = mpimg.imread(im_path).reshape((1, xdim*ydim*zdim))
+
+		x.append(img_pixels)
 		y.append(coord)
 
-	return x,y 
 
-def load_img(images):
-	"""
-		Most common image size 480 x 640
-	"""
-	minx, miny = 1000000, 1000000
-	img_size = {}
-	for i, im in enumerate(images):
-		if(i %100 == 0): print(i, len(images))
-		if(i > 1000): break
-		if(os.path.isfile(im)):
-			# if(i > 2): break 
-			try:
-				img = mpimg.imread(im)
-				xdim, ydim, zdim = img.shape
-				imsize = (xdim, ydim)
-				print(im, xdim, ydim)
-				if(imsize not in img_size):
-					img_size[imsize] = 0
-				img_size[imsize] += 1
-				if(xdim < minx): minx = xdim
-				if(ydim > miny): miny = ydim
-			except:
-				print("Invalid file")
-	print("Smallest Image: ", xdim, ydim)
-	return img_size
+	tot_num_points = num_train_pts + num_test_pts
+	if(num_test_pts > len(x)):
+		print("Data set does not have " + str(tot_num_points) + " data points.")
+		exit(1)
+
+	indices = range(len(x))
+	random.shuffle(indices)
+	train_indices, test_indices = indices[:num_train_pts], indices[num_train_pts:tot_num_points]
+	x_train, y_train = x[train_indices], y[train_indices]
+	x_test, y_test = x[test_indices], y[test_indices]
+
+	return x_train, y_train, x_test, y_test
 
 
-def driver():
-	x,y = get_data()
-	img_size = load_img(x)
-	print(img_size)
+def feedforward_nn():
+	### x_train has dimensions (1000, 640*480*3)
+	### y_train has dimensions (1000, 32)
+	x_train, y_train, x_test, y_test = get_data(1000, 200)
+
+	sess = tf.Session()
+	x = tf.placeholder(tf.float32, shape = [None, xdim*ydim*zdim])
+	y_ = tf.placeholder(tf.float32, shape = [None, output_len])
+	W = tf.Variable(tf.zeros[xdim*ydim*zdim, output_len])
+	b = tf.Variable(tf.zeros[output_len])
+	y = tf.nn.softmax(tf.matmul(x,W) + b)
+	cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
+
+	LEARNING_RATE = 0.1
+	TRAIN_STEPS = 2500
+	init = tf.global_variables_initializer()
+	sess.run(init)
+
+	training = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
+	correct_predict = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+	for i in range(TRAIN_STEPS+1):
+	    sess.run(training, feed_dict={x: x_train, y_: y_train})
+	    if i%100 == 0:
+	        print('Training Step:' + str(i) + '  Accuracy =  ' + str(sess.run(accuracy, feed_dict={x: x_test, y_: y_test})) + '  Loss = ' + str(sess.run(cross_entropy, {x: x_train, y_: y_train})))
+				
+
+
+# def load_img(images):
+# 	"""
+# 		Most common image size 480 x 640
+# 	"""
+# 	minx, miny = 1000000, 1000000
+# 	img_size = {}
+# 	for i, im in enumerate(images):
+# 		if(i %100 == 0): print(i, len(images))
+# 		if(i > 1000): break
+# 		if(os.path.isfile(im)):
+# 			# if(i > 2): break 
+# 			try:
+# 				img = mpimg.imread(im)
+# 				xdim, ydim, zdim = img.shape
+# 				imsize = (xdim, ydim)
+# 				print(im, xdim, ydim)
+# 				if(imsize not in img_size):
+# 					img_size[imsize] = 0
+# 				img_size[imsize] += 1
+# 				if(xdim < minx): minx = xdim
+# 				if(ydim > miny): miny = ydim
+# 			except:
+# 				print("Invalid file")
+# 	print("Smallest Image: ", xdim, ydim)
+# 	return img_size
+
+
+
+# def driver():
+# 	x,y = get_data()
+# 	img_size = load_img(x)
+# 	print(img_size)
 
 
 
 if __name__ == "__main__":
-	driver()
+	feedforward_nn()
