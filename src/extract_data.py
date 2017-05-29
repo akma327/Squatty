aa834e087a69d8a2139b0d1013710e726528bdfa
Gus Liu <gusliu@myth13.Stanford.EDU>
2017-05-29 11:02:25 -0700

    Resized images

---
+++ src/extract_data.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+import PIL
+
+def plot_images(data):
+  f = open('joint_annotation_data_scaled.txt', 'w')
+  for i in range(len(data)):
+    try:
+      line = data[i].split()
+      image_name = line[0]
+      img = mpimg.imread("../data/images/" + image_name)
+      line = line[1:]
+      x = line[:len(line) / 2]
+      y = line[len(line) / 2:]
+      
+      im = Image.open("../data/images/" + image_name)
+      size = 480, 640
+      im = im.resize(size, PIL.Image.ANTIALIAS)
+      im.save("../data/images_scaled/" + image_name)
+
+      newline = [image_name]
+      xdim, ydim, zdim = img.shape
+      for xval in x:
+        newline.append(str(float(xval) * size[0] / xdim))
+      for yval in y:
+        newline.append(str(float(yval) * size[1] / ydim))
+      newline = "\t".join(newline)
+      f.write(newline + "\n")
+    except:
+      print "Invalid file"
+
+
+def extract_data():
+  with open("../data/images/joint_annotation_data.txt") as f:
+    data = f.readlines()
+  plot_images(data)
+
+if __name__ == "__main__":
+  extract_data()
