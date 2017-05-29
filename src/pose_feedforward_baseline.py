a1f47643048c1ccd121a47865c360932100e32b0
Gus Liu <gusliu@corn09.stanford.edu>
2017-05-29 00:02:09 -0700

    Starting baseline implementation

---
+++ src/pose_feedforward_baseline.py
@@ -0,0 +1,24 @@
+# Author: Anthony Ma, Gus Liu
+# Email: akma327@stanford.edu, gusliu@stanford.edu
+# Date: 05/29/17
+# pose_feedforward_baseline.py
+
+
+
+
+USAGE_STR = """
+# Purpose
+# Implementation of single layer feedforward neural network baseline for pose estimation
+
+# Usage
+# python pose_feedforward_baseline.py 
+
+"""
+
+
+def driver():
+	pass
+
+
+if __name__ == "__main__":
+	driver()
